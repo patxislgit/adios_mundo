3c4dc2255a7e58eca0e72acb7b62df7eda319a68
Patxi Sanchis <patxi_sanchis@ieslasenia.org>
2019-09-18 23:58:11 +0200

    Experimentando con colores

--- AdiosMundo.java
+++ AdiosMundo.java
@@ -1,6 +1,9 @@
 public class AdiosMundo { // Clase principal
   public static void main (String[] args){
+    String amarillo = "\033[33m";
+    String verde = "\033[32m";
       System.out.println("¡Adios mundo!"); //Imprime el mensaje
       System.out.println("Pero... volveré");
+  }
 }
 
