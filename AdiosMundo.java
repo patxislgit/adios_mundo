ea89d74c16f9f061bdaf6f8ac1a15b26adc37b0a
Patxi Sanchis <patxi_sanchis@ieslasenia.org>
2019-09-18 19:27:54 +0200

    Modificados los dos ficheros

--- AdiosMundo.java
+++ AdiosMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo { // Clase principal
-  public satic void main (String[] args){
-      System.out.println("¡Adios mundo!");
+  public static void main (String[] args){
+      System.out.println("¡Adios mundo!"); //Imprime el mensaje
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # adios_mundo
 Programa en java que escribe ¡Adiós mundo!
+Realizado por Patxi Sanchis
