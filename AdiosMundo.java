4b25d4990e617592c8014bf51aa2fa721fb469b8
Patxi Sanchis <patxi_sanchis@ieslasenia.org>
2019-09-18 23:10:51 +0200

    Adios mundo y vuelta

--- AdiosMundo.java
+++ AdiosMundo.java
@@ -1,5 +1,6 @@
 public class AdiosMundo { // Clase principal
   public static void main (String[] args){
       System.out.println("¡Adios mundo!"); //Imprime el mensaje
-  }
+      System.out.println("Pero... volveré");
 }
+
