c065fce87ae7a135def8ee0a8ab3284549c968c6
Patxi Sanchis <patxi_sanchis@ieslasenia.org>
2019-09-19 09:46:16 +0200

    Mejorando los colores

--- AdiosMundo.java
+++ AdiosMundo.java
@@ -2,8 +2,9 @@ public class AdiosMundo { // Clase principal
   public static void main (String[] args){
     String amarillo = "\033[33m";
     String verde = "\033[32m";
-      System.out.println(amarillo + "¡Adios mundo!"); //Imprime el mensaje
-      System.out.println(verde + "Pero... volveré");
+    String reset = "\033[0m";
+      System.out.println(amarillo + "¡Adios mundo!" + reset); //Imprime el mensaje
+      System.out.println(verde + "Pero... volveré" + reset);
   }
 }
 
